1e4008a0488ac18f8c5295490ffe145b80014ce5
NadyCarboni <nady.n.m.c@gmail.com>
2021-06-02 08:54:05 -0300

    projeto modificado

--- src/Main/Components/CalcBody/index.jsx
+++ src/Main/Components/CalcBody/index.jsx
@@ -4,16 +4,24 @@ import React from "react";
 
 
 export const CalcBody = ()=> {
+  function click(e) {
+ e.value = target.value;
+    console.log(e.value)
+    }
   return (
-<div className="calculator">
+<div className="calculator ">
+
    <input className="visor" type="text" disabled="disabled"/>
-   <div className="numbers-calc">
-  
-   <div>7</div>
+   
+   <div className="numbers-calc t1-bg">
+
+
+      <div>7</div>
       <div>8</div>
-       <div>9</div>
-      <div>DEL</div>
-    <div>4</div>
+      <div>9</div>
+      <div onClick={click} value='del'>DEL</div>
+
+      <div>4</div>
       <div>5</div>
       <div>6</div>
       <div>+</div>
@@ -22,13 +30,17 @@ export const CalcBody = ()=> {
       <div>2</div>
       <div>1</div>
       <div>-</div>
-    <div>.</div>
+
+      <div>.</div>
       <div>0</div>
       <div>/</div>
       <div>x</div>
+
       <div className="reset">Reset</div>
        <div className="equal">=</div>
+</div>       
    </div>
-</div>
+
+
 );
 };
